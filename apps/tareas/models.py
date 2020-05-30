dec5033ffc9fceb286337a6ea126e3bba8d16c77
juanox <xjalbornoz@gmail.com>
2020-05-30 15:01:29 -0400

    modelo tareas modificado

--- apps/tareas/models.py
+++ apps/tareas/models.py
@@ -8,3 +8,9 @@ class Tareas (models.Model):
     duracion = models.IntegerField(default=0)
     lugar = models.CharField(max_length=50)
     encargado = models.ForeignKey(Usuario,null=True,blank=True,on_delete=models.CASCADE)
+    fecha_creacion= models.DateField(auto_now_add=True)
+    comentarios = models.CharField(max_length=200, blank=True)
+    completada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre
