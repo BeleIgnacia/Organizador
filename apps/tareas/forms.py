3be3be02a636ef4ef899f23257b46f6a8359c39a
juanox <xjalbornoz@gmail.com>
2020-05-31 04:42:11 -0400

    ayura :c con la vista/formulario de la asignacion de las tareas

--- apps/tareas/forms.py
+++ apps/tareas/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from apps.tareas.models import Tareas
 
 class TareaForm(forms.ModelForm):
-    
+
 
     class Meta:
         model = Tareas
@@ -11,18 +11,24 @@ class TareaForm(forms.ModelForm):
             'nombre',
             'complejidad',
             'duracion',
-            'encargado',
+            'lugar',
+            'comentarios'
+    #        'encargado',
         ]
         labels =  {
             'nombre' : 'Nombre',
             'complejidad' : 'Complejidad',
             'duracion' : 'Duración en horas',
-            'encargado': 'Encargado',
+            'lugar' : 'Dependencia',
+            'comentarios' : 'Comentarios',
+    #        'encargado': 'Encargado',
         }
 
         widgets = {
             'nombre':forms.TextInput(attrs={'class':'form-control'}),
             'complejidad':forms.NumberInput(attrs={'class':'custom-range','type':'range','max':5,'min':1,'step':1,'list':'tickmarks'}),
             'duracion':forms.TextInput(attrs={'class':'form-control'}),
-            'encargado':forms.Select(attrs={'class':'form-control'}),
+            'lugar':forms.TextInput(attrs={'class':'form-control'}),
+            'comentarios':forms.TextInput(attrs={'class':'form-control'}),
+        #    'encargado':forms.Select(attrs={'class':'form-control'}),
         }
--- apps/tareas/urls.py
+++ apps/tareas/urls.py
@@ -1,7 +1,9 @@
+from apps.tareas.views import crear_tarea,  listar_tareas, asignar_tarea
 from django.urls import path
-from apps.tareas.views import TareasCreate, listar_tareas
+
 
 urlpatterns = [
-    path('tarea_form/', TareasCreate.as_view(), name='tarea_form'),
-    path('listar_tareas/', listar_tareas, name='listar_tareas')
+    path('tarea_form/', crear_tarea.as_view(), name='tarea_form'),
+    path('listar_tareas/', listar_tareas.as_view(), name='listar_tareas'),
+    path('asignar_tarea/', asignar_tarea.as_view(), name='asignar_tarea'),
 ]
