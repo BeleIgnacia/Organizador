e253606e52abcc7abc76321d5b84799acb5d1e5b
AmigoManuel <diazulloamanuel@gmail.com>
2020-06-18 18:35:56 -0400

    Pequeñas modificaciones en la interfaz

--- apps/interfaz/views.py
+++ apps/interfaz/views.py
@@ -5,32 +5,12 @@ from random import randrange
 class Home(TemplateView):
 	template_name = 'interfaz/home.html'
 
-	def get_context_data(self,**kwargs):
-		context = super(Home, self).get_context_data(**kwargs)
-		context['content'] = "home_content"
-		return context
-
 class Nosotros(TemplateView):
-	template_name = 'interfaz/home.html'
-
-	def get_context_data(self,**kwargs):
-		context = super(Nosotros, self).get_context_data(**kwargs)
-		context['content'] = "nosotros_content"
-		return context
+	template_name = 'interfaz/nosotros.html'
 
 class Ayuda(TemplateView):
-	template_name = 'interfaz/home.html'
-
-	def get_context_data(self,**kwargs):
-		context = super(Ayuda, self).get_context_data(**kwargs)
-		context['content'] = "ayuda_content"
-		return context
+	template_name = 'interfaz/ayuda.html'
 
 class Contacto(TemplateView):
-	template_name = 'interfaz/home.html'
-
-	def get_context_data(self,**kwargs):
-		context = super(Contacto, self).get_context_data(**kwargs)
-		context['content'] = "contacto_content"
-		return context
+	template_name = 'interfaz/contacto.html'
 
--- organizador/settings.py
+++ organizador/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'organizador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -107,7 +107,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
