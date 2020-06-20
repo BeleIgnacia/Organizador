a0e7b3593fca4c4c6be79e0698dbaa93a4da5e31
AmigoManuel <diazulloamanuel@gmail.com>
2020-06-20 17:46:52 -0400

    Añadidos modelos de Dependencia y PerteneceDependencia a hogar.

--- apps/hogar/models.py
+++ apps/hogar/models.py
@@ -12,6 +12,27 @@ class Domicilio(models.Model):
     def __str__(self):
         return '{} {}, {}, {}'.format(self.calle, self.numero, self.comuna, self.ciudad)
 
+# Usuario crea sus dependencias
+# Todas las que quiera
+# Luego ve si las asigna
+class Dependencia(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+# Son un tanto redundantes estos modelos
+# Pero su estructura es necesaria para los formularios
+# Para consultar por las dependencias de un domicilio es
+# pd = PerteneceDependencia.objects.filter(domicilio=self.usuario.domicilio,asignada=True)
+# pd es un queryset
+class PerteneceDependencia(models.Model):
+    dependencia = models.ForeignKey(Dependencia, on_delete=models.CASCADE)
+    domicilio = models.ForeignKey(Domicilio, on_delete=models.CASCADE)
+    asignada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{}'.format(self.dependencia)
 
 class Usuario(User):
     user = models.OneToOneField(User, parent_link=True, on_delete=models.CASCADE)
