b3e110f69ddcc183dfa51b56d48596fb10c8d896
AmigoManuel <diazulloamanuel@gmail.com>
2020-06-29 14:00:12 -0400

    Añadido testing de urls en app de hogar

---
+++ apps/hogar/tests/test_urls.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from apps.hogar.views import *
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_dashboard_url_is_resolved(self):
+        url = reverse('hogar:dashboard')
+        self.assertEquals(resolve(url).func.view_class, Dashboard)
+
+    def test_list_usuarios_url_is_resolved(self):
+        url = reverse('hogar:list_usuarios')
+        self.assertEquals(resolve(url).func.view_class, Usuariolist)
+
+    def test_añadir_url_is_resolved(self):
+        url = reverse('hogar:añadir')
+        self.assertEquals(resolve(url).func.view_class, RegisterUser)
+
+    def test_domicilio_modificar_url_is_resolved(self):
+        url = reverse('hogar:domicilio_modificar')
+        self.assertEquals(resolve(url).func.view_class, DomicilioModificar)
+
+    def test_domicilio_dependencias_url_is_resolved(self):
+        url = reverse('hogar:domicilio_dependencias')
+        self.assertEquals(resolve(url).func.view_class, DomicilioDependencias)
