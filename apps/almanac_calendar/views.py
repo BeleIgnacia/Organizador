61ea2c8ba5519f572082782013c501df40fa1a75
AmigoManuel <diazulloamanuel@gmail.com>
2020-06-30 10:12:25 -0400

    Formulario de evento funcional y filtrado de eventos según tareas.

--- apps/almanac_calendar/views.py
+++ apps/almanac_calendar/views.py
@@ -19,23 +19,28 @@ class MostrarCalendario(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(MostrarCalendario, self).get_context_data(**kwargs)
-        all_events = Event.objects.all()
-        events = eval(serializers.serialize("json", all_events))
-        events = list(map(lambda x: x['fields'], events))
 
-        #Este procedimiento filtra las tareas asignadas al domicilio
-        #Pero no filtra las que ya se candelarizaron
         self.usuario = Usuario.objects.get(pk=self.request.session['pk_usuario'])
+        # Este procedimiento filtra las tareas asignadas al domicilio
+        # Pero no filtra las que ya se candelarizaron
         self.usuarios = Usuario.objects.filter(domicilio=self.usuario.domicilio)
         self.tareas = AsignarTarea.objects.filter(usuario__in=self.usuarios)
         context['form'].fields['asignar_tarea'].queryset = self.tareas
 
-        context['events'] = events
+        self.events = Event.objects.filter(asignar_tarea__in=self.tareas)
+        self.events = eval(serializers.serialize("json", self.events))
+        self.events = list(map(lambda x: x['fields'], self.events))
+
+        context['events'] = self.events
         return context
 
     def form_valid(self, form):
         instance = form.save(commit=False)
-        print("dira hola cuando la form sea valida")
+        instance.title = instance.asignar_tarea.tarea.nombre
+        instance.end = instance.start + instance.asignar_tarea.tarea.duracion
+        print(instance.start)
+        print(instance.end)
+        instance.save()
         return HttpResponseRedirect(reverse_lazy('calendario:mostrar_calendario'))
 
 
